41fd550697c1d392f3f7ee8c2fbab14f7dbe0572
dikshaadgitm123 <gargdiksha177@gmail.com>
2024-07-06 16:05:49 +0530

    update import field

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import pdf from "../pdf/resume.pdf";
+import pdf from "../pdf/Diksha-Resume.pdf";
 import hero from "./data/hero.json";
 import Typed from "typed.js";
 
